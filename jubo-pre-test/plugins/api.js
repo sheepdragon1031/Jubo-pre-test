c1e4318789fc18817788a785f6ecd7e88de1cb68
sheepdragon <dragonsheep1031@gmail.com>
2022-07-14 02:00:25 +0800

    feat: api CRUD function and layout

---
+++ jubo-pre-test/plugins/api.js
@@ -0,0 +1,63 @@
+import axios from "axios"
+
+export const getList = async (path) => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/${path}`
+    )
+    return res
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+export const getItem = async (path, id) => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/${path}/${id}?populate=orders`
+    )
+    return res
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+export const postItem = async (path, data) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/${path}`,
+      {
+        data: data
+      }
+    )
+    return res
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+export const DeleItem = async (path, id) => {
+  try {
+    const res = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/${path}/${id}`
+    )
+    return res
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
+export const putItem = async (path, id, data) => {
+  try {
+    const res = await axios.put(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/${path}/${id}`,
+      {
+        data: data
+      }
+    )
+    return res
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
