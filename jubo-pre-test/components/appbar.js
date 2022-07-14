4670140b9dbe71131997f45b7365569cccc5cd7f
sheepdragon <dragonsheep1031@gmail.com>
2022-07-14 23:54:11 +0800

    feat: border-radius

--- jubo-pre-test/components/appbar.js
+++ jubo-pre-test/components/appbar.js
@@ -8,22 +8,25 @@ import IconButton from "@mui/material/IconButton"
 
 const Appbar = ({}) => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            JuBo
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar
+      position="static"
+      sx={{
+        borderRadius: 1
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        ></IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          JuBo
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
 
